25a280280c446471ca67bb8e8a0870d7615dfdf0
YanTsinovoy <yand199627@gmail.com>
2019-03-04 22:02:08 +0200

    lol

---
+++ src/Pages/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SignIn from "../../Components/SignIn/SignIn.js"
+
+let Auth = p => {
+  return(
+    <div>
+      <SignIn />
+    </div>
+  )
+}
+
+export default Auth
